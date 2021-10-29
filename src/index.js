2015cb309898fa93a4570e3bad43bfd297c7545b
nsendoda <nsendoda@gmail.com>
2021-10-29 19:37:10 +0900

    ディレクトリとファイル整理、htmlファイル編集

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('taskapp')
 );
+
 
 reportWebVitals();
